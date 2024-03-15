dceb1bcd84058453f36517cf5d3920d4bb0ebf8d
Quang <20378quang@gmail.com>
2024-03-15 17:04:28 -0400

    commit 03/15/2024 5:04pm revised

--- image2ascii.py
+++ image2ascii.py
@@ -5,57 +5,42 @@ author: Quang Huynh
 """
 
 from PIL import Image
-def convertToAscii(img, type, saveas, scale):
-    scale = int(scale)  # convert scale to integer
+def convertToAscii(img, saveas, scale):
+    scale = int(scale)  # Ensure scale is an integer
 
-    image_file = Image.open(img)  # open image file
-    w, h = image_file.size  # image file size (width & height)
+    # Open the image and convert to grayscale
+    image_file = Image.open(img).convert('L')  # 'L' mode converts image to grayscale
+    w, h = image_file.size
 
-    image_file.resize((w//scale, h//scale).save("resized.%s" % type))
-
-    image_file = Image.open("resized.%s" % type)  # open new image
-    w, h = image_file.size  # new width & height
-
-    grid = []
-    for i in range(h):
-        grid.append(["X"] * w)
-
+    # Resize image according to the scale factor
+    image_file = image_file.resize((w // scale, h // scale))
     px_coord = image_file.load()
 
-    for y in range(h):  # iterate over height
-        for x in range(w):  # iterate over x
-            # pixel brightness to char
-            if sum(px_coord[x, y]) == 0:
-                grid[y][x] = "#"
-            elif sum(px_coord[x, y]) in range (1, 100):
-                grid[y][x] = "X"
-            elif sum(px_coord[x, y]) in range(100, 200):
-                grid[y][x] = "%"
-            elif sum(px_coord[x,y]) in range(200, 300):
-                grid[y][x] = "&"
-            elif sum(px_coord[x][y]) in range(300, 400):
-                grid[y][x] = "*"
-            elif sum(px_coord[x][y]) in range(400, 500):
-                grid[y][x] = "+"
-            elif sum(px_coord[x][y]) in range(500, 600):
-                grid[y][x] = "/"
-            elif sum(px_coord[x][y]) in range(600, 700):
-                grid[y][x] = "("
-            elif sum(px_coord[x][y]) in range(700, 750):
-                grid[y][x] = "'"
-            else:
-                grid[y][x] = " "
+    ascii_chars = "@%#*+=-:. "  # Define a string of ASCII characters from dark to light
+    ascii_len = len(ascii_chars)
 
+    # Create ASCII art
+    ascii_art = []
+    for y in range(h // scale):
+        line = []
+        for x in range(w // scale):
+            brightness = px_coord[x, y]  # Brightness value in 'L' mode is an integer
+            # Map the brightness value to an ASCII char
+            char_index = brightness * (ascii_len - 1) // 255
+            line.append(ascii_chars[char_index])
+        ascii_art.append("".join(line))
+
+    # Write the ASCII art to the specified file
     with open(saveas, "w") as f:
-        for row in grid:
-            f.write("".join(row) + "\n")
+        for line in ascii_art:
+            f.write(line + "\n")
 
 def main():
     print("Welcome to Quang's Image to Text program!")
     image = input("\tInput image path: ")
     type = input("\tInput image type: ")
     saveas = input("\tWhere do you want your text file to be saved?: ")
-    scale = input("Scale of image: ")
+    scale = input("\tScale of image: ")
     convertToAscii(image, type, saveas, scale)
     print("Done!")
 
